4bff740f145b527e9dd2b8c2015996b7812a2947
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-09-05 22:18:46 -0300

    feat: mostrar lista de elementos

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
 
-function getImage($name) {
-    $images = array(
-        "https://cdn.prod.website-files.com/5f5a53e153805db840dae2db/64e79ca5aff2fb7295bfddf9_github-que-es.jpg",
-        "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OgR4jtnczW-hZUCB-gOg_g.png",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbsuQQUD-CKSk5-Pw6zmAPPO7_4CtaarQ7_g&s"
-    );
-    if($name == "github") return $images[0];
-    if($name == "gitlab") return $images[1];
-    if($name == "bitbucket") return $images[2];
-    return "https://edteam-media.s3.amazonaws.com/blogs/big/2ab53939-9b50-47dd-b56e-38d4ba3cc0f0.png";
-}
+$github = (object) [
+    'name' => 'github',
+    'title' => "Github",
+    'link' => "https://github.com",
+    'text' => "Build and ship software on a single, collaborative platform",
+    'url' => "https://cdn.prod.website-files.com/5f5a53e153805db840dae2db/64e79ca5aff2fb7295bfddf9_github-que-es.jpg",
+];
 
+$gitlab = (object) [
+    "name" => 'gitlab',
+    "title" => 'Gitlab',
+    'link' => "https://gitlab.com",
+    'text' => 'Build software, not toolchains. With native AI at every step.',
+    'url' => "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OgR4jtnczW-hZUCB-gOg_g.png"
+];
+
+$bitbucket = (object) [
+    "name" => "bitbucket",
+    "title" => "Bitbucket",
+    'link' => "https://bitbucket.org",
+    'text' => "Code and CI/CD, powered by the Atlassian platform",
+    "url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbsuQQUD-CKSk5-Pw6zmAPPO7_4CtaarQ7_g&s"
+];
+
+
+$gitProviders = [$github, $gitlab, $bitbucket];
+
+$cardsCount =  count($gitProviders);
+//echo var_dump($gitProviders);
 
 $cssLibrary = "bootstrap";
 $pageTitle = "Programación Web II";
@@ -44,14 +61,20 @@ $pageTitle = "Programación Web II";
         echo $pageTitle; 
         ?>
     </h1>
-
+    <?php /* 1 - 3 */ ?>
+    <?php for($i = 0; $i < $cardsCount; $i++){ ?>
     <div class="card" style="width: 18rem;">
-        <img src="<?= getImage("bitbucket") ?>" class="card-img-top" alt="...">
+        <img src="<?= $gitProviders[$i]->url ?>" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">
+                <?= $gitProviders[$i]->title ?>
+            </h5>
+            <p class="card-text">
+                <?= $gitProviders[$i]->text ?>
+            </p>
+            <a href="<?= $gitProviders[$i]->link ?>" class="btn btn-primary">Ir a web</a>
         </div>
     </div>
+    <?php } ?>
 </body>
 </html>
