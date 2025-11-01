2439695ec3a1787ada88e08f1365886ab698023d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-31 22:20:30 -0300

    feat: agregar items usando DOM

---
+++ components/footer.php
@@ -0,0 +1,56 @@
+<script>
+    const angularJs = {
+        "name": "angularjs",
+        "title": "AngularJS",
+        "link": "https://angularjs.org/",
+        "text": "AngularJS support has officially ended as of January 2022. See what ending support means and read the end of life announcement. Visit angular.io for the actively supported Angular.",
+        "url": "https://angularjs.org/img/angularjs-for-header-only.svg",
+        "topic": "web_framework"
+    }
+
+    const gitlab = {
+        "name": 'gitlab',
+        "title": 'Gitlab',
+        "link": "https://gitlab.com",
+        "text": "Build software, not toolchains. With native AI at every step.",
+        "url": "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OgR4jtnczW-hZUCB-gOg_g.png"
+    }
+    gitlab["topic"] = "git_provider";
+
+    const parent = document.querySelector("#post_container");
+
+
+    const child = document.createElement("div");
+    child.className = "card";
+    child.style.width = '18rem';
+    parent.appendChild(child);
+    child.innerHTML = `
+        <img src="${gitlab.url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">
+                ${gitlab.title}
+            </h5>
+            <p class="card-text">
+                ${gitlab.text}
+            </p>
+            <a href="${gitlab.link}" class="btn btn-primary">Ir a web</a>
+        </div>
+    `;
+
+    const otherChild = document.createElement("div");
+    otherChild.className = "card";
+    otherChild.style.width = "18rem";
+    parent.appendChild(otherChild);
+    otherChild.innerHTML = `
+        <img src="${angularJs.url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">
+                ${angularJs.title}
+            </h5>
+            <p class="card-text">
+                ${angularJs.text}
+            </p>
+            <a href="${angularJs.link}" class="btn btn-primary">Ir a web</a>
+        </div>
+    `;
+</script>
--- views/home.view.php
+++ views/home.view.php
@@ -1,7 +1,7 @@
 <h1> <?= $pageTitle ?>: <?= $topic ?> </h1>
 
 <div class="container">
-    <div class="row">
+    <div class="row" id="post_container">
         <?php for ($i = 0; $i < count($list); $i++) {
             require("./components/card.php");
         } ?>
