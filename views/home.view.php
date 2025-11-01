35c0c6249a5ec3fb40da08b72517049f3a8fce96
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-31 22:50:58 -0300

    refactor: eliminar objectos sin uso

--- views/home.view.php
+++ views/home.view.php
@@ -1,9 +1,11 @@
+
+
 <h1> <?= $pageTitle ?>: <?= $topic ?> </h1>
 
 <div class="container">
     <div class="row" id="post_container">
-        <?php for ($i = 0; $i < count($list); $i++) {
+        <!--?php for ($i = 0; $i < count($list); $i++) {
             require("./components/card.php");
-        } ?>
+        } ?-->
     </div>
 </div>
