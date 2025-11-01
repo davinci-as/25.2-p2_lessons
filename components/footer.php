4662a711a27b0d393dc194cdae30b096a636f672
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-31 22:36:52 -0300

    feat: mover objetos a array

--- components/footer.php
+++ components/footer.php
@@ -19,25 +19,30 @@
 
     }
 
-    const angularJs = {
+    const items = [
+        {
         "name": "angularjs",
         "title": "AngularJS",
         "link": "https://angularjs.org/",
         "text": "AngularJS support has officially ended as of January 2022. See what ending support means and read the end of life announcement. Visit angular.io for the actively supported Angular.",
         "url": "https://angularjs.org/img/angularjs-for-header-only.svg",
         "topic": "web_framework"
-    }
-
-    const gitlab = {
+    }, {
         "name": 'gitlab',
         "title": 'Gitlab',
         "link": "https://gitlab.com",
         "text": "Build software, not toolchains. With native AI at every step.",
-        "url": "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OgR4jtnczW-hZUCB-gOg_g.png"
+        "url": "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*OgR4jtnczW-hZUCB-gOg_g.png",
+        "topic": "git_provider"
+    }, {
+        "name": "bitbucket",
+        "title": "Bitbucket",
+        "link": "https://bitbucket.org/product/",
+        "text": "Code and CI/CD, powered by the Atlassian platform",
+        "url": "https://dam-cdn.atl.orangelogic.com/AssetLink/np0j8d765ebc4a778s3tdr6bc7avc301/fl_keep_metadata/logo-light_Bitbucket_mark-tile_inverse_RGB.svg",
+        "topic": "git_provider"
     }
-    gitlab["topic"] = "git_provider";
-
-    const items = [angularJs, gitlab];
+    ];
 
     const parent = document.querySelector("#post_container");
 
